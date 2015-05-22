4a0a2c1dacc2635cbbfb2870ebde187ebfb49e73
Vu Tran <baysao@gmail.com>
2015-05-22 11:22:20 +0700

    add helpers

--- mekit.js
+++ mekit.js
@@ -2,7 +2,10 @@ var deepjson = require( 'deep-json' );
 var util = require('util');
 var jf = require("jsonfile");
 var myArgs = process.argv.slice(2);
-
+if(myArgs.length != 2) {
+    console.log("node mekit [input_json_file] [output_json_file]");
+    process.exit(0);
+}
 var jfinal = deepjson(myArgs[0]);
 console.log(util.inspect(jfinal, false, null));
 var fileout = myArgs[1];
